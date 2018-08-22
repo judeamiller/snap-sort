e1be768a1798cfd100fe5f9222713dac99f8839e
Jude Baca-Miller <judeamiller@gmail.com>
2018-08-22 08:56:11 -0600

    remove console log, add comments

--- scripts.js
+++ scripts.js
@@ -7,9 +7,9 @@ function alphabetize() {
 	//remove the sensors
 	let newWords= words.filter(a => a !=="sensors");
 
+	//alphabetize
 	newWords.sort();
 
-	console.log(newWords);
-		//join the randomized words and insert new paragraph
-		document.getElementById("star-par").textContent = newWords.join(" ");
+	//join the randomized words and insert new paragraph
+	document.getElementById("star-par").textContent = newWords.join(" ");
 }
