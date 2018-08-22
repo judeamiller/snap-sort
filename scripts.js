81fc3894e1e718b9331a6171918093bc5a4e9e94
Jude Baca-Miller <judeamiller@gmail.com>
2018-08-22 11:46:18 -0600

    edit function to sort case insensitive.

--- scripts.js
+++ scripts.js
@@ -2,13 +2,15 @@ function alphabetize() {
 	let text = document.getElementById("star-par").textContent;
 
 	//make an array of words based on empty spaces change all words to lowercase
-	let words = text.toLowerCase().split(" ");
+	let words = text.split(" ");
 
 	//remove the sensors
 	let newWords= words.filter(a => a !=="sensors");
 
 	//alphabetize
-	newWords.sort();
+	newWords.sort(function (a, b) {
+		return a.toLowerCase().localeCompare(b.toLowerCase());
+	});
 
 	//join the randomized words and insert new paragraph
 	document.getElementById("star-par").textContent = newWords.join(" ");
