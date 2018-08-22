0470666559c1939d6ffbfe2742026ab632790082
Jude Baca-Miller <judeamiller@gmail.com>
2018-08-22 11:56:19 -0600

    allow finding of case insensitive "sensors."

--- scripts.js
+++ scripts.js
@@ -4,13 +4,14 @@ function alphabetize() {
 	//make an array of words based on empty spaces change all words to lowercase
 	let words = text.split(" ");
 
-	//remove the sensors
-	let newWords= words.filter(a => a !=="sensors");
-
 	//alphabetize
-	newWords.sort(function (a, b) {
+	words.sort(function (a, b) {
 		return a.toLowerCase().localeCompare(b.toLowerCase());
 	});
+
+	//remove the sensors
+	let newWords= words.filter(a => a.toLowerCase() !=="sensors");
+
 
 	//join the randomized words and insert new paragraph
 	document.getElementById("star-par").textContent = newWords.join(" ");
